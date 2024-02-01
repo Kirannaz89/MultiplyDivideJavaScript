1551ac1d48f265e6d0cb79c29e3d9dc2768a53c2
Kirannaz89 <Nazkiran1989.gmail.com>
2024-02-01 19:11:58 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+function multiply(){
+ 
+  var N1 = document.getElementById("num1").value;
+  var N2 = document.getElementById("num2").value;
+
+  var R1 = N1*N2
+
+  document.getElementById("result").innerHTML = R1;
+
+}
+
+function divide(){
+ 
+  var N1 = document.getElementById("num1").value;
+  var N2 = document.getElementById("num2").value;
+
+  var R1 = N1/N2
+
+  document.getElementById("result").innerHTML = R1;
+
+}
